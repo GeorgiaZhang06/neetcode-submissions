7a1f43d869c4bb006c44a807b8e098094293bf7c
Georgia Zhang <georgiazhang88@gmail.com>
2026-09-23 11:50:29 -0400

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t reverseN = 0;
+        int bitIndex=0;
+        while (n!=0){
+            if ((n&1) ==1){
+                reverseN |= (1 << (31-bitIndex));
+            }
+                n = n>>1;
+                bitIndex++;
+
+        }
+        return reverseN;
+    }
+};
